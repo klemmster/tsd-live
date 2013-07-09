f68a239e3fb4346217db1a25b68cec4d6d3813d6
Richard Klemm <richy@coding-reality.de>
2013-07-09 09:59:49 +0200

    Initial Commit

---
+++ CCS/msp430g2xx3_dco_flashcal/Release/msp430g2xx3_dco_flashcal.pp
@@ -0,0 +1,11 @@
+# FIXED
+
+msp430g2xx3_dco_flashcal.obj: ../msp430g2xx3_dco_flashcal.c
+msp430g2xx3_dco_flashcal.obj: C:/ti/ccsv5/ccs_base/msp430/include/msp430.h
+msp430g2xx3_dco_flashcal.obj: C:/ti/ccsv5/ccs_base/msp430/include/msp430g2553.h
+msp430g2xx3_dco_flashcal.obj: C:/ti/ccsv5/ccs_base/msp430/include/in430.h
+
+../msp430g2xx3_dco_flashcal.c: 
+C:/ti/ccsv5/ccs_base/msp430/include/msp430.h: 
+C:/ti/ccsv5/ccs_base/msp430/include/msp430g2553.h: 
+C:/ti/ccsv5/ccs_base/msp430/include/in430.h: 
